7d13dee9a4feffbb84899bf494c554957df431bb
Indy Iri <indy.iri@student.pxl.be>
2025-02-24 09:50:25 +0100

    wpf aangemaakt

---
+++ H2Oef1Woordenboek/H2Oef1Woordenboek/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace H2Oef1Woordenboek;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
